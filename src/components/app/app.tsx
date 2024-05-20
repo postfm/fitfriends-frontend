e09e510eb4428ad9022ca9b13afa744ed27440ad
postfm <72159645+postfm@users.noreply.github.com>
2024-05-20 20:53:31 +0300

    Добавит Ract Query DevTools в проект

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from '../../router/router';
 import { HelmetProvider } from 'react-helmet-async';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -10,6 +11,7 @@ export default function App(): JSX.Element {
     <HelmetProvider>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />;
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </HelmetProvider>
   );
